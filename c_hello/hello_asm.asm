d70e4f84d13e05566b90346bf56ccba884ee204f
ed <ed@inl.ics.keio.ac.jp>
2020-01-13 02:02:59 +0900

    fix linux asm

--- c_hello/hello_asm.asm
+++ c_hello/hello_asm.asm
@@ -1,16 +1,20 @@
 bits 64
 
 %ifdef MACOS
-  %define SYS_WRITE 0x2000004
-  %define SYS_EXIT  0x2000001
+  %define SYS_WRITE   0x2000004
+  %define SYS_EXIT    0x2000001
+  %define hello_func  _hello
+  %define exit_func   _my_exit
 %else
-  %define SYS_WRITE 1
-  %define SYS_EXIT  60
+  %define SYS_WRITE   1
+  %define SYS_EXIT    60
+  %define hello_func  hello
+  %define exit_func   my_exit
 %endif
 
-global _hello
+global hello_func
 
-_hello:
+hello_func:
   mov rdx, rsi
   mov rsi, rdi
   mov rax, SYS_WRITE
@@ -18,9 +22,9 @@ _hello:
   syscall
   ret
 
-global _my_exit
+global exit_func
 
-_my_exit:
+exit_func:
   mov rsi, rdi
   mov rax, SYS_EXIT
   syscall
@@ -31,9 +35,9 @@ _my_exit:
   _start:
     mov rdi, str_hello
     mov rsi, length
-    call _hello
+    call hello_func
     mov rdi, 1
-    call _my_exit
+    call exit_func
 
   SECTION .data
     str_hello db "Hello World", 0x0a
